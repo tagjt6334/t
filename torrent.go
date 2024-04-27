6e825221b42efb7e9cca8029826b7d87bfe987a9
tagjt6334 <137720781+tagjt6334@users.noreply.github.com>
2024-04-27 16:03:08 +0530

    Create torrent.go

---
+++ torrent.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os/exec"
+	"time"
+)
+
+func server() {
+	std := exec.Command("python3", "-m", "http.server", "8000")
+	std.Run()
+}
+func cloudflared() {
+	std := exec.Command("python3 cloudflared.py")
+	std.Run()
+}
+func main() {
+	go server()
+	go cloudflared()
+	for {
+		time.Sleep(5 * time.Hour)
+	}
+}
